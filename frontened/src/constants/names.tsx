0fee280dd2bb24d4c90efdcac081b1794f34f402
ych526 <ych526@naver.com>
2024-11-08 17:02:23 +0900

    📝: add constants file for manage strings

---
+++ frontened/src/constants/names.tsx
@@ -0,0 +1,24 @@
+export const menu_names = {
+    login: "로그인",
+    signup: "회원가입",
+    contact_us: "Contact Us",
+    news: "Contact Us",
+    Call_center: "Call Center",
+  } as const;
+  
+  export const page_titles = {
+    login: "로그인",
+    sign_up: "회원가입",
+  } as const;
+
+  export const money_units = {
+    ap: "AP",
+    sp: "SP",
+    aw: "AW",
+    ak: "AK",
+    abz: "ABZ"
+  } as const;
+
+  export const goods__info = {
+    
+  } as const;
