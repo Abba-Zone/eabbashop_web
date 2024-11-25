13e3c1bc4bc06d3c7ad0acac8a66a54c5d6d3348
rudgns9334 <rudgns9334@gmail.com>
2024-11-25 16:27:52 +0900

    🚀 signup logic clear

---
+++ backend/abba/src/main/java/com/zon/abba/members/request/SignupRequest.java
@@ -0,0 +1,34 @@
+package com.zon.abba.members.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class SignupRequest {
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("provider")
+    private String provider;
+
+    @JsonProperty("phone")
+    private String phone;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("recommend")
+    private String recommend;
+}
---
+++ backend/abba/src/main/java/com/zon/abba/members/service/SignupService.java
@@ -0,0 +1,46 @@
+package com.zon.abba.members.service;
+
+import com.zon.abba.members.entity.Member;
+import com.zon.abba.members.repository.MemberRepository;
+import com.zon.abba.members.request.LoginRequest;
+import com.zon.abba.members.request.SignupRequest;
+import com.zon.abba.members.response.LoginResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SignupService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SignupService.class);
+    private final MemberRepository memberRepository;
+    private final LoginService loginService;
+
+    @Transactional
+    public LoginResponse signup(SignupRequest signupRequest){
+        // 1. 받아온 유저 정보를 입력한다.
+        Member member = Member.builder()
+                .firstName(signupRequest.getFirstName())
+                .lastName(signupRequest.getLastName())
+                .email(signupRequest.getEmail())
+                .provider(signupRequest.getProvider())
+                .phone(signupRequest.getPhone())
+                .password(signupRequest.getPassword())
+                .recommendId(signupRequest.getRecommend())
+                .country("KOR")
+                .build();
+
+        member.perPersist();
+        memberRepository.save(member);
+
+        // 로그인 정보를 담아서 로그인 신호를 보낸다.
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail(signupRequest.getEmail());
+        loginRequest.setPassword(signupRequest.getPassword());
+
+        return loginService.login(loginRequest);
+    }
+}
