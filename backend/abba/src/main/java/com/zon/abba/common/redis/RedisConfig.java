f6b45220f76a803b64010b65fe23f421e8030ff9
rudgns9334 <rudgns9334@gmail.com>
2024-11-29 02:24:10 +0900

    🚀 update redis setting

--- backend/abba/src/main/java/com/zon/abba/common/redis/RedisConfig.java
+++ backend/abba/src/main/java/com/zon/abba/common/redis/RedisConfig.java
@@ -37,6 +37,7 @@ public class RedisConfig {
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory());
         return template;
     }
 }
