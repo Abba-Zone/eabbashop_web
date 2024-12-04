c6e1f664f5f923fd186537f324c0a1720ce6cc21
JHT <jeonht043@gmail.com>
2024-12-05 06:08:29 +0900

    🎨LAYOUT:push basic css to BottomButton

--- frontend/src/components/common/list/BottomButton.tsx
+++ frontend/src/components/common/list/BottomButton.tsx
@@ -1,19 +1,16 @@
-import { useState } from "react";
-
+import "./style.css";
 interface OwnProps{
     lastPage:number,
     nowPage:number,
     changePage(move:string):void
 }
-type style = {
-    color: string;
-}
+
 const BottomButton:React.FC<OwnProps> = (props) => {
     const rendering = (): JSX.Element[] => {
         const result = [];
         const firstNum = props.nowPage - (props.nowPage - 1) % 5
         for(let i = 0 ; i < 5 && firstNum + i <= props.lastPage ; i++){
-            result.push(<div style={props.nowPage===firstNum + i?{backgroundColor:'gray'} : {}} key={i} onClick={() => {props.changePage((firstNum + i).toString())}}>{firstNum + i}</div>);
+            result.push(<div className={props.nowPage===firstNum + i?"select-page-number-button":"none-select-page-number-button"} key={i} onClick={() => {props.changePage((firstNum + i).toString())}}>{firstNum + i}</div>);
         }
         return result;
     }
@@ -38,12 +35,12 @@ const BottomButton:React.FC<OwnProps> = (props) => {
         props.changePage('last')
     }
     return (
-      <div>
-          <div style={props.nowPage === 1?{color:"gray"}:{}} onClick={clickFirst}>gotoFirst </div>
-          <div style={props.nowPage === 1?{color:"gray"}:{}} onClick={clickPre}>gotoPre</div>
+      <div className="bottom-button-set">
+          <div className={props.nowPage === 1?"page-move-button-first-page":"page-move-button-any-page"} onClick={clickFirst}>gotoFirst </div>
+          <div className={props.nowPage === 1?"page-move-button-first-page":"page-move-button-any-page"} onClick={clickPre}>gotoPre</div>
           {rendering()}
-          <div style={props.nowPage === props.lastPage?{color:"gray"}:{}} onClick={clickNext}>gotoNext</div>
-          <div style={props.nowPage === props.lastPage?{color:"gray"}:{}} onClick={clickLast}>gotoLast</div>
+          <div className={props.nowPage === props.lastPage?"page-move-button-last-page":"page-move-button-any-page"} onClick={clickNext}>gotoNext</div>
+          <div className={props.nowPage === props.lastPage?"page-move-button-last-page":"page-move-button-any-page"} onClick={clickLast}>gotoLast</div>
       </div>
     );
   }
