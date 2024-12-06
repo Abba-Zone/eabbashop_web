c5a32fcf50e32fbb52b04519c9314e6e68821575
JHT <jeonht043@gmail.com>
2024-12-06 12:39:16 +0900

    🔨MODIFY: Improve Some Functions

--- frontend/src/components/common/list/BottomButton.tsx
+++ frontend/src/components/common/list/BottomButton.tsx
@@ -2,45 +2,45 @@ import "./style.css";
 interface OwnProps{
     lastPage:number,
     nowPage:number,
-    changePage(move:string):void
+    changePage(move:number):void
 }
 
-const BottomButton:React.FC<OwnProps> = (props) => {
+const BottomButton:React.FC<OwnProps> = ({lastPage,nowPage,changePage}) => {
     const rendering = (): JSX.Element[] => {
         const result = [];
-        const firstNum = props.nowPage - (props.nowPage - 1) % 5
-        for(let i = 0 ; i < 5 && firstNum + i <= props.lastPage ; i++){
-            result.push(<div className={props.nowPage===firstNum + i?"select-page-number-button":"none-select-page-number-button"} key={i} onClick={() => {props.changePage((firstNum + i).toString())}}>{firstNum + i}</div>);
+        const firstNum = nowPage - (nowPage - 1) % 5
+        for(let i = 0 ; i < 5 && firstNum + i <= lastPage ; i++){
+            result.push(<div className={nowPage===firstNum + i?"select-page-number-button":"none-select-page-number-button"} key={i} onClick={() => {changePage(firstNum + i)}}>{firstNum + i}</div>);
         }
         return result;
     }
     const clickFirst =()=>{
-        if (props.nowPage===1)
+        if (nowPage===1)
             return; 
-        props.changePage('first');
+        changePage(1);
     }
     const clickPre =()=>{
-        if (props.nowPage===1)
+        if (nowPage===1)
             return; 
-        props.changePage('pre');
+        changePage(nowPage - 1);
     }
     const clickNext =()=>{
-        if (props.nowPage===props.lastPage)
+        if (nowPage===lastPage)
             return;
-        props.changePage('next')
+        changePage(nowPage + 1)
     }
     const clickLast =()=>{
-        if (props.nowPage===props.lastPage)
+        if (nowPage===lastPage)
             return;
-        props.changePage('last')
+        changePage(lastPage)
     }
     return (
       <div className="bottom-button-set">
-          <div className={props.nowPage === 1?"page-move-button-first-page":"page-move-button-any-page"} onClick={clickFirst}>gotoFirst </div>
-          <div className={props.nowPage === 1?"page-move-button-first-page":"page-move-button-any-page"} onClick={clickPre}>gotoPre</div>
+          <div className={nowPage === 1?"page-move-button-first-page":"page-move-button-any-page"} onClick={clickFirst}>gotoFirst </div>
+          <div className={nowPage === 1?"page-move-button-first-page":"page-move-button-any-page"} onClick={clickPre}>gotoPre</div>
           {rendering()}
-          <div className={props.nowPage === props.lastPage?"page-move-button-last-page":"page-move-button-any-page"} onClick={clickNext}>gotoNext</div>
-          <div className={props.nowPage === props.lastPage?"page-move-button-last-page":"page-move-button-any-page"} onClick={clickLast}>gotoLast</div>
+          <div className={nowPage === lastPage?"page-move-button-last-page":"page-move-button-any-page"} onClick={clickNext}>gotoNext</div>
+          <div className={nowPage === lastPage?"page-move-button-last-page":"page-move-button-any-page"} onClick={clickLast}>gotoLast</div>
       </div>
     );
   }
