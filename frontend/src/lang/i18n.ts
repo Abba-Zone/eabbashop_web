5211ad6dee592bfee34c1db208cf8a97aade5342
JHT <jeonht043@gmail.com>
2024-12-06 12:41:39 +0900

    🚀FEAT:Language Setting For InquiryLsitPage

--- frontend/src/lang/i18n.ts
+++ frontend/src/lang/i18n.ts
@@ -5,15 +5,21 @@ import AdminMenu_en from './en/AdminMenu.json';
 import AdminMenu_ko from './ko/AdminMenu.json';
 import AdminManagerMember_en from './en/AdminManagerMember.json';
 import AdminManagerMember_ko from './ko/AdminManagerMember.json';
+import AdminInquiry_ko from './ko/AdminInquiry.json';
+import AdminInquiry_en from './ko/AdminInquiry.json';
+
+
 
 const resources = {
   ko: {
     AdminMenu: AdminMenu_ko,
     AdminManagerMember: AdminManagerMember_ko,
+    AdminInquiry: AdminInquiry_ko,
   },
   en: {
     AdminMenu: AdminMenu_en,
     AdminManagerMember: AdminManagerMember_en,
+    AdminInquiry: AdminInquiry_en,
   },
 };
 
