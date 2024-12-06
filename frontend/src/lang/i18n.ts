c065367ee9d75b47de4ad3e2e2cee646a83f7f0c
Cheetozzeong <ych526@naver.com>
2024-12-06 12:18:04 +0900

    🚀: add english, japanese, chinese language set in i18next

--- frontend/src/lang/i18n.ts
+++ frontend/src/lang/i18n.ts
@@ -3,8 +3,13 @@ import { initReactI18next } from 'react-i18next';
 
 import AdminMenu_en from './en/AdminMenu.json';
 import AdminMenu_ko from './ko/AdminMenu.json';
+import AdminMenu_ja from './ja/AdminMenu.json';
+import AdminMenu_zh from './zh/AdminMenu.json';
+
 import AdminManagerMember_en from './en/AdminManagerMember.json';
 import AdminManagerMember_ko from './ko/AdminManagerMember.json';
+import AdminManagerMember_ja from './ja/AdminManagerMember.json';
+import AdminManagerMember_zh from './zh/AdminManagerMember.json';
 
 const resources = {
   ko: {
@@ -15,11 +20,19 @@ const resources = {
     AdminMenu: AdminMenu_en,
     AdminManagerMember: AdminManagerMember_en,
   },
+  ja: {
+    AdminMenu: AdminMenu_ja,
+    AdminManagerMember: AdminManagerMember_ja,
+  },
+  zh: {
+    AdminMenu: AdminMenu_zh,
+    AdminManagerMember: AdminManagerMember_zh,
+  },
 };
 
 i18n.use(initReactI18next).init({
   resources: resources,
-  lng: 'en',
+  lng: 'ko',
   fallbackLng: 'en',
   ns: ['AdminMenu'],
   defaultNS: 'AdminMenu',
