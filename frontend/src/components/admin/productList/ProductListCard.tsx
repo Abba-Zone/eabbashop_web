741f2800fe79357f09bc5f6f0f6099a139ac1909
JHT <jeonht043@gmail.com>
2024-12-09 11:35:37 +0900

    🚀FEAT: Develop ProductListCard

---
+++ frontend/src/components/admin/productList/ProductListCard.tsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
+interface Props{
+  product:product;
+  }
+  const ProductListCard:React.FC<Props> = ({product}) => {
+      const navigate = useNavigate();
+      return (
+        <tr onClick={()=>{navigate(`/admin/customer-inquiry/${product.productID}`)}}>
+          <td>선택</td>
+          <td>{product.name}</td>
+          <td>{product.seller}</td>
+          <td>{product.stock}</td>
+          <td>{product.activeYN? "활성화" : "비활성화"}</td>
+        </tr>
+      );
+}
+    
+export default ProductListCard;
+    
