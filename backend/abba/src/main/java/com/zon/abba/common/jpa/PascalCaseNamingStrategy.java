09c1764b8d11b47f9976d85daee978860c68583b
rudgns9334 <rudgns9334@gmail.com>
2024-12-09 17:34:19 +0900

    🚀 mysql setting pascalcase

---
+++ backend/abba/src/main/java/com/zon/abba/common/jpa/PascalCaseNamingStrategy.java
@@ -0,0 +1,34 @@
+package com.zon.abba.common.jpa;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class PascalCaseNamingStrategy implements PhysicalNamingStrategy {
+
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return name;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return name;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return name;
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return name;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment jdbcEnvironment) {
+        return name;
+    }
+}
+
