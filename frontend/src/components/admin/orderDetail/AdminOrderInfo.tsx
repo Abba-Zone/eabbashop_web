b9a4eaea701a19bf23a4676321102853ffb91936
JHT <jeonht043@gmail.com>
2024-12-11 00:56:58 +0900

    🚀FEAT: Set Language

--- frontend/src/components/admin/orderDetail/AdminOrderInfo.tsx
+++ frontend/src/components/admin/orderDetail/AdminOrderInfo.tsx
@@ -1,15 +1,18 @@
+import { useTranslation } from "react-i18next";
+
 interface Props{
     order:orderOrder,
   }
   const AdminOrderInfo:React.FC<Props> = ({order}) => {
+    const { t } = useTranslation();
       return (
         <div>
-          <h3>AdminOrderInfo</h3>
-          <div><div>상품명</div><div>{order.productName}</div></div>
-          <div><div>개수</div><div>{order.quantity}</div></div>
-          <div><div>배송상태</div><div>{order.status}</div></div>
-          <div><div>결제방식</div><div>{order.결제방식}</div></div>
-          <div><div>주문일시</div><div>{order.createdDateTime}</div></div>
+          <h3>{t("AdminOrder:Detail.Item01.Title")}</h3>
+          <div><div>{t("AdminOrder:Detail.Item01.Attribute01")}</div><div>{order.productName}</div></div>
+          <div><div>{t("AdminOrder:Detail.Item01.Attribute02")}</div><div>{order.quantity}</div></div>
+          <div><div>{t("AdminOrder:Detail.Item01.Attribute03")}</div><div>{order.status}</div></div>
+          <div><div>{t("AdminOrder:Detail.Item01.Attribute04")}</div><div>{order.결제방식}</div></div>
+          <div><div>{t("AdminOrder:Detail.Item01.Attribute05")}</div><div>{order.createdDateTime}</div></div>
         </div>
       );
   }
