a331b5663574a520b17727a3f3ad65f57b3785de
JHT <jeonht043@gmail.com>
2024-12-11 00:38:53 +0900

    🚀FEAT: Connect Apis And Page

--- frontend/src/services/sale.ts
+++ frontend/src/services/sale.ts
@@ -1,5 +1,9 @@
-import { getOrderList } from '../apis/sale'
+import { getOrderList, getOrderDetail } from '../apis/sale'
 
 export const getOrderList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<orderList> => {
     return await getOrderList(pageNo, pageSize, filter, filterValue, sort, sortValue);
+};
+
+export const getOrderDetail_s = async (orderID:string): Promise<orderDetail> => {
+    return await getOrderDetail(orderID);
 };
--- frontend/src/types/sale.d.ts
+++ frontend/src/types/sale.d.ts
@@ -10,3 +10,43 @@ interface orderList{
     totalOrder : number,
     orders : order[]
 }
+
+interface orderDetail{
+    product : orderProduct,
+    order : orderOrder,
+    address : orderAddress,
+    member : orderMember
+}
+interface orderProduct{
+    thumbnail : string,
+    productName : striong,
+    taxFreePrice : number,
+    SPPrice : number,
+    realPrice : number,
+    allowNation : string[],
+    viewSite : string
+}
+interface orderOrder{
+    productName : string,
+    quantity : 12,
+    status : string,
+    결제방식: string,
+    createdDateTime : string
+}
+interface orderAddress{
+    name : string,
+    phone : string,
+    zipCode: string,
+    baseAddress: string,
+    detailAddress: string,
+    billZipCode : string,
+    billBaseAddress : string,
+    billDetailAddress : string,
+    comment : string
+}
+interface orderMember{
+    name : string,
+    email : string,
+    role : string,
+    grade : string
+}
