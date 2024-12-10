bfac64ef9b95ff706f1f39b8bbcb3c9bba75eaa0
JHT <jeonht043@gmail.com>
2024-12-10 11:20:36 +0900

    🚀FEAT:Connect TestApis To Page

--- frontend/src/services/product.ts
+++ frontend/src/services/product.ts
@@ -1,4 +1,12 @@
-import { getlist } from '../apis/productApi'
+import { getProductList, getProductDetail, getProductReviewList } from '../apis/productApi'
 export const getProductList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<productList> => {
-    return await getlist(pageNo, pageSize, filter, filterValue, sort, sortValue);
+    return await getProductList(pageNo, pageSize, filter, filterValue, sort, sortValue);
+};
+
+export const getProductDetail_s = async (productId:string): Promise<productDetailAndSeller> => {
+    return await getProductDetail(productId);
+};
+
+export const getProductReviewList_s = async (pageNo:number, pageSize:number, sort:number, productID:string): Promise<reviewList> => {
+    return await getProductReviewList(pageNo, pageSize, sort, productID);
 };
