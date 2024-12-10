ea25308f0f02b7bca255eb59740af0728612b7ef
JHT <jeonht043@gmail.com>
2024-12-11 01:56:40 +0900

    🚀FEAT: Develop Components For InvoiceListPage

---
+++ frontend/src/components/admin/invoiceList/InvoiceistCard.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+interface Props{
+  invoice:invoice;
+  }
+const InvoiceListCard:React.FC<Props> = ({invoice}) => {
+    const navigate = useNavigate();
+    return (
+      <tr onClick={()=>{navigate(`/admin/invoicedetail/${invoice.invoiceID}`)}}>
+        <td>선택</td>
+        <td>{invoice.orderDetailID}</td>
+        <td>{invoice.invoiceNo}</td>
+        <td>{invoice.name}</td>
+        <td>{invoice.status}</td>
+        <td>{invoice.createdDateTime}</td>
+      </tr>
+    );
+}
+    
+export default InvoiceListCard;
+    
