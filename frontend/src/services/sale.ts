1c43d36004d736d89d691a96eb2b53fd18dc2edf
JHT <jeonht043@gmail.com>
2024-12-11 02:16:53 +0900

    🚀FEAT: Connect Apis And Page

--- frontend/src/services/sale.ts
+++ frontend/src/services/sale.ts
@@ -1,9 +1,16 @@
-import { getOrderList, getOrderDetail } from '../apis/sale'
+import { getOrderList, getOrderDetail, getInvoiceList } from '../apis/sale'
 
+
+/* Order */
 export const getOrderList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<orderList> => {
     return await getOrderList(pageNo, pageSize, filter, filterValue, sort, sortValue);
 };
 
 export const getOrderDetail_s = async (orderID:string): Promise<orderDetail> => {
     return await getOrderDetail(orderID);
+};
+
+/* Invoice */
+export const getInvoiceList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<invoiceList> => {
+    return await getInvoiceList(pageNo, pageSize, filter, filterValue, sort, sortValue);
 };
