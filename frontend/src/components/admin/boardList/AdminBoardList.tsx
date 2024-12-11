98ac8109a8be3ef0a9dc5bc971eb0a164a4eae85
JHT <jeonht043@gmail.com>
2024-12-11 23:04:54 +0900

    🚀FEAT: Set Language To LetterListPage

--- frontend/src/components/admin/boardList/AdminBoardList.tsx
+++ frontend/src/components/admin/boardList/AdminBoardList.tsx
@@ -18,11 +18,11 @@ const AdminBoardList:React.FC<Props> = ({boards, changeSort}) => {
     const result = 
     <tr>
       <th>선택</th>
-      <th onClick={()=>{changeSort('title')}}>{t("AdminBoard:NoticeList.Filter01")}</th>
-      <th onClick={()=>{changeSort('name')}}>{t("AdminBoard:NoticeList.Filter02")}</th>
-      <th onClick={()=>{changeSort('TopYN')}}>{t("AdminBoard:NoticeList.Filter03")}</th>
-      <th onClick={()=>{changeSort('ShowYN')}}>{t("AdminBoard:NoticeList.Filter04")}</th>
-      <th onClick={()=>{changeSort('createdDateTime')}}>{t("AdminBoard:NoticeList.Filter05")}</th>
+      <th onClick={()=>{changeSort('title')}}>{t("AdminBoard:List.Filter01")}</th>
+      <th onClick={()=>{changeSort('name')}}>{t("AdminBoard:List.Filter02")}</th>
+      <th onClick={()=>{changeSort('TopYN')}}>{t("AdminBoard:List.Filter03")}</th>
+      <th onClick={()=>{changeSort('ShowYN')}}>{t("AdminBoard:List.Filter04")}</th>
+      <th onClick={()=>{changeSort('createdDateTime')}}>{t("AdminBoard:List.Filter05")}</th>
     </tr>;
     return result;
   }
