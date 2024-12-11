259d00baa614e8bf9a02d7f394a835f34325e067
JHT <jeonht043@gmail.com>
2024-12-12 02:27:30 +0900

    🚀FEAT:Set Language

--- frontend/src/components/admin/boardDetail/AdminBoardInfo.tsx
+++ frontend/src/components/admin/boardDetail/AdminBoardInfo.tsx
@@ -1,15 +1,18 @@
+import { useTranslation } from "react-i18next";
+
 interface Props{
   board:boardDetail,
 }
 const AdminBoardInfo:React.FC<Props> = ({board}) => {
+    const { t } = useTranslation();
     return (
       <div>
-        <div><div>게시글유형</div><div>{board.type}</div></div>
-        <div><div>작성자</div><div>{board.name}</div></div>
-        <div><div>작성일</div><div>{board.createdDateTime}</div></div>
-        <div><div>상세내용</div><div>{board.contents}</div></div>
-        <div><div>상단고정</div><div>{board.topYN}</div></div>
-        <div><div>활성화</div><div>{board.showYN}</div></div>
+        <div><div>{t("AdminBoard:Detail.Item01.Attribute01")}</div><div>{board.type}</div></div>
+        <div><div>{t("AdminBoard:Detail.Item01.Attribute02")}</div><div>{board.name}</div></div>
+        <div><div>{t("AdminBoard:Detail.Item01.Attribute03")}</div><div>{board.createdDateTime}</div></div>
+        <div><div>{t("AdminBoard:Detail.Item01.Attribute04")}</div><div>{board.contents}</div></div>
+        <div><div>{t("AdminBoard:Detail.Item01.Attribute05")}</div><div>{board.topYN}</div></div>
+        <div><div>{t("AdminBoard:Detail.Item01.Attribute06")}</div><div>{board.showYN}</div></div>
       </div>
     );
 }
--- frontend/src/pages/admin/AdminBoardDetail.tsx
+++ frontend/src/pages/admin/AdminBoardDetail.tsx
@@ -2,35 +2,41 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getBoardDetail_s } from '../../services/board';
 import { AdminBoardInfo } from '../../components';
+import { useTranslation } from 'react-i18next';
+
 const AdminBoardDetail: React.FC = () => {
-  const [board, setBoard] = useState<boardDetail | undefined>(undefined);
-  const params = useParams<{id:string}>();
-  const getBoardDetail = useCallback (async () => {
-    try {
-      if (params.id !== undefined){
-        const boardDetail : boardDetail = await getBoardDetail_s(params.id);
-        setBoard(boardDetail);
-      }
-    } catch (error) {
-      console.error('Error fetching boardDetail:', error);
-    }
-  }, [params.id]); ;
-  useEffect(() => {
+    const { t } = useTranslation();
+    const [board, setBoard] = useState<boardDetail | undefined>(undefined);
+    const params = useParams<{id:string}>();
+    const getBoardDetail = useCallback (async () => {
+        try {
+            if (params.id !== undefined){
+                const boardDetail : boardDetail = await getBoardDetail_s(params.id);
+                setBoard(boardDetail);
+            }
+        } catch (error) {
+            console.error('Error fetching boardDetail:', error);
+        }
+    }, [params.id]);
+
+    useEffect(() => {
     getBoardDetail(); // 비동기 함수 호출
-  }, [getBoardDetail]);
-  if (!board) {
+    }, [getBoardDetail]);
+
+    if (!board) {
+        return (
+            <div>
+                <h1>{t("AdminBoard:Detail.Option.Attribute00")}</h1>
+            </div>
+        );
+    }
+
     return (
-      <div>
-        <h1>게시글이 없습니다.</h1>
-      </div>
+        <div>
+            <h1>{board.title}</h1>
+            <AdminBoardInfo board={board}></AdminBoardInfo>
+        </div>
     );
-  }
-  return (
-    <div>
-      <h1>{board.title}</h1>
-      <AdminBoardInfo board={board}></AdminBoardInfo>
-    </div>
-  );
 };
 
 export default AdminBoardDetail;
