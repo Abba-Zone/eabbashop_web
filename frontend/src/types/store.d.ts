5fa3e2daa7d00cd63a127e42716c525e5ea02540
JHT <jeonht043@gmail.com>
2024-12-13 01:12:48 +0900

    🚀FEAT: Connect TestApi And Page

--- frontend/src/types/store.d.ts
+++ frontend/src/types/store.d.ts
@@ -8,5 +8,21 @@ interface store{
 
 interface storeList{
     totalStore : number,
+    stores : store[]
+}
+
+interface storeInfo extends store{
+    zipCode : string,
+    baseAddress : string,
+    detailAddress : string
+}
+
+interface storeProduct{
+    totalStore : number,
     store : store[]
+}
+
+interface storeProductList{
+    totalStore : number,
+    stores : storeProduct[]
 }
