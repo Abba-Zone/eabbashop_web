6afa06a7e3fef66494551f93d2abfcf2ae743fd4
JHT <jeonht043@gmail.com>
2024-12-13 01:12:48 +0900

    🚀FEAT: Connect TestApi And Page

--- frontend/src/apis/store.ts
+++ frontend/src/apis/store.ts
@@ -1,6 +1,5 @@
 import { getData, postData, getTestData} from './mainApi'
-
-export const getlist = (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string):storeList => {
+export const getStoreList = (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string):storeList => {
     /* real code*/
     // getData<storeList>('/list?' + 'pageNo='+ pageNo + '&pageSize='+ pageSize + '&filter='+ filter + '&filterValue='+ filterValue + '&sort='+ sort+ '&sortValue='+ sortValue)
     //     .then((data:APIResponse<storeList>) => {
@@ -12,13 +11,56 @@ export const getlist = (pageNo:number, pageSize:number, filter:number, filterVal
     /* make for test*/
     var result :storeList = {
         totalStore : 10003,
-        store:[
+        stores:[
             {storeID : "1asbebxcvbxcvbcb", name : "매장명예시입니다", host : "전 현태A", phone : "010-1234-5678", createdDateTime : "2024-11-21 11:53:11"},
             {storeID : "2asbebxcvbxcvbcb", name : "매장명예시입니다", host : "전 현태A", phone : "010-1234-5678", createdDateTime : "2024-11-21 11:53:11"},
             {storeID : "3asbebxcvbxcvbcb", name : "매장명예시입니다", host : "전 현태A", phone : "010-1234-5678", createdDateTime : "2024-11-21 11:53:11"},
             {storeID : "4asbebxcvbxcvbcb", name : "매장명예시입니다", host : "전 현태A", phone : "010-1234-5678", createdDateTime : "2024-11-21 11:53:11"},
             {storeID : "5asbebxcvbxcvbcb", name : "매장명예시입니다", host : "전 현태A", phone : "010-1234-5678", createdDateTime : "2024-11-21 11:53:11"}
         ]
+    };
+    return result;
+}
+
+export const getStoreProductList = (storeID:string, pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string):productList => {
+    /* real code*/
+    // getData<storeList>('/list?' + 'pageNo='+ pageNo + '&pageSize='+ pageSize + '&filter='+ filter + '&filterValue='+ filterValue + '&sort='+ sort+ '&sortValue='+ sortValue)
+    //     .then((data:APIResponse<storeList>) => {
+    //         return data.result;
+    //     }
+    // );
+    // return null as unknown as storeList;
+
+    /* make for test*/
+    var result :productList = {
+        totalProduct: 123412414,
+        products : [
+            {productID: "1dvxb32c45s7d87b49lkfdvb", name : "물병",seller : "전 현태",	stock : 100, activeYN : true,},
+            {productID: "2dvxb32c45s7d87b49lkfdvb", name : "물병",seller : "전 현태",	stock : 100, activeYN : false,},
+            {productID: "3dvxb32c45s7d87b49lkfdvb", name : "물병",seller : "전 현태",	stock : 100, activeYN : false,},
+            {productID: "4dvxb32c45s7d87b49lkfdvb", name : "물병",seller : "전 현태",	stock : 100, activeYN : true,}
+        ]
+    };
+    return result;
+}
+
+export const getStoreDetail = (productID:string):storeInfo => {
+    // getData<storeInfo>('/store/detail?productID='+ productID)
+    //     .then((data:APIResponse<storeInfo>) => {
+    //         return data.result;
+    //     }
+    // );
+    // return null as unknown as storeInfo;
+    
+    var result:storeInfo= {
+        storeID : "1asbebxcvbxcvbcb",
+		name : "가게이름?",
+		host:"정 경훈C",
+		phone : "02-123-1234",
+		zipCode : "11111",
+		baseAddress : "부산시 기장군",
+		detailAddress : "파란하늘집",
+		createdDateTime :"2024-12-12 13:25:33"
     };
     return result;
 }
--- frontend/src/services/store.ts
+++ frontend/src/services/store.ts
@@ -1,5 +1,13 @@
-import { getlist } from '../apis/store'
+import { getStoreList, getStoreProductList, getStoreDetail} from '../apis/store'
 
 export const getStoreList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<storeList> => {
-    return await getlist(pageNo, pageSize, filter, filterValue, sort, sortValue,);
+    return await getStoreList(pageNo, pageSize, filter, filterValue, sort, sortValue,);
+};
+
+export const getStoreProductList_s = async (storeID:string, pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<productList> => {
+    return await getStoreProductList(storeID, pageNo, pageSize, filter, filterValue, sort, sortValue,);
+};
+
+export const getStoreDetail_s = async (storeID:string): Promise<storeInfo> => {
+    return await getStoreDetail(storeID);
 };
