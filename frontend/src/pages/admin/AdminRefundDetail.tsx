ca5fd1ab159cd48b2e2d5d80f9a3f37eaf74b62e
JHT <jeonht043@gmail.com>
2024-12-13 16:00:03 +0900

    🚀FEAT: Add RefundDetailPage For RefundListPage

---
+++ frontend/src/pages/admin/AdminRefundDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminRefundDetail: React.FC = () => {
+  return (
+    <div>
+      <h1>AdminRefundDetail</h1>
+    </div>
+  );
+};
+
+export default AdminRefundDetail;
