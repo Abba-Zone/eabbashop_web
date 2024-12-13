ef380ae8fa1297409271326e6bff13137ae7559a
rudgns9334 <rudgns9334@gmail.com>
2024-12-13 17:36:12 +0900

    🚀 create getWallet logic

---
+++ backend/abba/src/main/java/com/zon/abba/account/entity/Wallet.java
@@ -0,0 +1,80 @@
+package com.zon.abba.account.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Wallet")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "WalletID", columnDefinition = "CHAR(36)", nullable = false, updatable = false)
+    private String walletId;
+
+    @Column(name = "MemberID", columnDefinition = "CHAR(36)", nullable = false)
+    private String memberId;
+
+    @Column(name = "LP", precision = 10, scale = 2, nullable = false)
+    private BigDecimal lp = BigDecimal.ZERO;
+
+    @Column(name = "AK", precision = 10, scale = 2, nullable = false)
+    private BigDecimal ak = BigDecimal.ZERO;
+
+    @Column(name = "ABZ", precision = 10, scale = 2, nullable = false)
+    private BigDecimal abz = BigDecimal.ZERO;
+
+    @Column(name = "ABZPoint", precision = 10, scale = 2, nullable = false)
+    private BigDecimal abzPoint = BigDecimal.ZERO;
+
+    @Column(name = "AP", precision = 10, scale = 2, nullable = false)
+    private BigDecimal ap = BigDecimal.ZERO;
+
+    @Column(name = "SP", precision = 10, scale = 2, nullable = false)
+    private BigDecimal sp = BigDecimal.ZERO;
+
+    @Column(name = "CreatedID", columnDefinition = "CHAR(36)", nullable = false)
+    private String createdId;
+
+    @Column(name = "ModifiedID", columnDefinition = "CHAR(36)")
+    private String modifiedId;
+
+    @CreationTimestamp
+    @Column(name = "CreatedDateTime", nullable = false, updatable = false)
+    private LocalDateTime createdDateTime;
+
+    @UpdateTimestamp
+    @Column(name = "ModifiedDateTime")
+    private LocalDateTime modifiedDateTime;
+
+    @Column(name = "DeleteYN", columnDefinition = "CHAR(1) DEFAULT 'N'", nullable = false)
+    private String deleteYN = "N";
+
+    @Column(name = "ActiveYN", columnDefinition = "CHAR(1) DEFAULT 'Y'", nullable = false)
+    private String activeYN = "Y";
+
+    @PrePersist
+    public void perPersist(){
+
+        if (this.createdId == null) this.createdId = UUID.randomUUID().toString();
+        if (this.modifiedId == null) this.modifiedId = UUID.randomUUID().toString();
+
+        if(this.createdDateTime == null) this.createdDateTime = LocalDateTime.now();
+        if(this.modifiedDateTime == null) this.modifiedDateTime = LocalDateTime.now();
+
+        if(this.deleteYN == null) this.deleteYN = "N";
+        if(this.activeYN == null) this.activeYN = "Y";
+    }
+}
