81553cebb4779f927166effb6d28999a0d41b129
JHT <jeonht043@gmail.com>
2024-12-13 17:27:09 +0900

    🚀FEAT: Connect TestApi And InquiryPage

--- frontend/src/services/inquiry.ts
+++ frontend/src/services/inquiry.ts
@@ -1,8 +1,8 @@
-import { getlist, getInfo } from '../apis/inquiry'
+import { getInquiryList, getinquiryDetail } from '../apis/inquiry'
 
 export const getInquiryList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<inquiryList> => {
-    return await getlist(pageNo, pageSize, filter, filterValue, sort, sortValue);
+    return await getInquiryList(pageNo, pageSize, filter, filterValue, sort, sortValue);
 };
 export const getinquiryDetail_s = async (inquiryID:string): Promise<inquiryDetail> => {
-    return await getInfo(inquiryID);
+    return await getinquiryDetail(inquiryID);
 };
