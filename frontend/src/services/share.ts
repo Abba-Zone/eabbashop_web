b1f21e2b32e2aff4af2908054129b0f2bf5b9b22
JHT <jeonht043@gmail.com>
2024-12-13 18:57:47 +0900

    🚀FEAT: Connect TestApi And ShareMoney

--- frontend/src/services/share.ts
+++ frontend/src/services/share.ts
@@ -1,9 +1,12 @@
-import { getShareLineList, getShareMoneyDetailList } from '../apis/shareApi'
+import { getShareLineList, getShareMoneyList, getShareMoneyDetailList } from '../apis/shareApi'
 /* ShareLine */
 export const getShareLineList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<shareLineList> => {
     return await getShareLineList(pageNo, pageSize, filter, filterValue, sort, sortValue);
 };
 /* ShareMoney */
+export const getShareMoneyList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<shareMoneyList> => {
+    return await getShareMoneyList(pageNo, pageSize, filter, filterValue, sort, sortValue);
+};
 /* ShareMoneyDetail */
 export const getShareMoneyDetailList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<shareMoneyDetailList> => {
     return await getShareMoneyDetailList(pageNo, pageSize, filter, filterValue, sort, sortValue);
