43cdaee0d414355b86160fc36aa2b230874ed621
JHT <jeonht043@gmail.com>
2024-12-13 16:12:48 +0900

    🚀FEAT: Apply Components To ShareLineListPage

--- frontend/src/pages/admin/AdminShareLineList.tsx
+++ frontend/src/pages/admin/AdminShareLineList.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { AdminShareLineListComponent, BottomButton, SearchSet } from '../../components';
 
 const AdminShareLineList: React.FC = () => {
+  const [shareLines, setShareLines] = useState<shareLine[]>([]);
+  const [pageNo, setPageNo] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [lastPage, setLastPage] = useState<number>(1);
+  const [filter, setFilter] = useState<number>(1);
+  const [filterValue, setFilterValue] = useState<string>("");
+  const [sort, setSort] = useState<string>("createdDateTime");
+  const [sortValue, setSortValue] = useState<string>("DESC");
+  const selectList: { select: string, selectName: string, selectType:string, itemList:string[]}[] = 
+  [
+    {selectName:'상품명', select:'name', selectType:'text', itemList:[]},
+    {selectName:'판매자', select:'seller', selectType:'text', itemList:[]},
+    {selectName:'재고', select:'stock', selectType:'text', itemList:[]},
+    {selectName:'활성화', select:'activeYN', selectType:'select', itemList:['활성화', '비활성화']},
+  ];
+  const getShareLineList = useCallback( async () => {
+      try {
+        // const total_and_productList : productList = await getProductList_s(pageNo, pageSize, filter, filterValue, sort, sortValue);
+        // setProducts(total_and_productList.products);
+        // setLastPage(total_and_productList.totalProduct === 0? 1:Math.floor((total_and_productList.totalProduct - 1)/pageSize) + 1);
+      } catch (error) {
+        console.error('Error fetching shareLine list:', error);
+      }
+  },[pageNo, pageSize, filter, filterValue, sort, sortValue]);
+
+  const changePage = (move:number) =>{
+        setPageNo(move);
+  }
+  const changeSort = (sortName:string) => {
+    if (sortName === sort){
+      if(sortValue ==='ASC')
+        setSortValue('DESC')
+      else
+      setSortValue('ASC')
+    } else {
+      setSort(sortName);
+      setSortValue('ASC');
+    }
+  }
+  const changeFilter = (key:number, value:string) =>{
+    setFilter(key);
+    setFilterValue(value);
+  }
+
+  useEffect(() => {
+    getShareLineList(); // 비동기 함수 호출
+    }, [getShareLineList]);
   return (
     <div>
-      <h1>AdminShareLineList</h1>
+      <h1>수당라인</h1>
+      <SearchSet selectList={selectList} searchClick={changeFilter}></SearchSet>
+      <AdminShareLineListComponent shareLines={shareLines} changeSort={changeSort}/>
+      <BottomButton lastPage={lastPage} nowPage={pageNo} changePage={changePage}></BottomButton>
     </div>
   );
-};
+}
 
 export default AdminShareLineList;
