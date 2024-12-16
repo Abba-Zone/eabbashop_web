2bd812b732ad95940f3f3aa61a73334d02aecd9a
rudgns9334 <rudgns9334@gmail.com>
2024-12-16 15:55:22 +0900

    🚀 setting cors https://abbazon.global

--- backend/abba/src/main/java/com/zon/abba/common/cors/WebConfig.java
+++ backend/abba/src/main/java/com/zon/abba/common/cors/WebConfig.java
@@ -2,6 +2,7 @@ package com.zon.abba.common.cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNullApi;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,10 +12,11 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                        .allowedOrigins("http://localhost:3000") // 모든 도메인 허용
+                        .allowedOrigins("http://localhost:3000", "https://abbazon.global") // 모든 도메인 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true); // 인증 정보 허용 (쿠키, Authorization 헤더 등)
