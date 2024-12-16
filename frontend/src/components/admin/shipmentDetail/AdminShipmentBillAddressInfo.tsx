73153e60ffa3694196ab2270f9027462f236549f
JHT <jeonht043@gmail.com>
2024-12-16 17:16:48 +0900

    🚀FEAT: Set Language For AdminShipmentPages

--- frontend/src/components/admin/shipmentDetail/AdminShipmentBillAddressInfo.tsx
+++ frontend/src/components/admin/shipmentDetail/AdminShipmentBillAddressInfo.tsx
@@ -7,7 +7,7 @@ const AdminShipmentBillAddressInfo:React.FC<Props> = ({address}) => {
     const { t } = useTranslation();
     return (
         <div>
-            <h3>청구서 주소</h3>
+            <h3>{t("AdminShipment:Detail.Item04.Title")}</h3>
             <div><div>{t("AdminShipment:Detail.Item04.Attribute01")}</div><div>{address.zipCode}</div></div>
             <div><div>{t("AdminShipment:Detail.Item04.Attribute02")}</div><div>{address.baseAddress}</div></div>
             <div><div>{t("AdminShipment:Detail.Item04.Attribute03")}</div><div>{address.detailAddress}</div></div>
