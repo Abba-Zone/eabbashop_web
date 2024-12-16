3db445866639415e1026078013f24bc7270788d0
JHT <jeonht043@gmail.com>
2024-12-16 17:14:20 +0900

    🚀FEAT: Set Language For AdminShareMoneyPages

--- frontend/src/components/admin/shareMoneyDetailList/AdminShareMoneyDetailListComponent.tsx
+++ frontend/src/components/admin/shareMoneyDetailList/AdminShareMoneyDetailListComponent.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import ListCard from "./AdminShareMoneyDetailList";
 interface Props{
   shareMoneyDetails:shareMoneyDetail[],
@@ -5,6 +6,7 @@ interface Props{
 }
 
 const AdminShareMoneyDetailListComponent:React.FC<Props> = ({shareMoneyDetails, changeSort}) => {
+  const { t } = useTranslation();
   const rendering = (): JSX.Element[] => {
       const result = [];
       for(let i = 0 ; i < shareMoneyDetails.length; i++){
@@ -16,12 +18,12 @@ const AdminShareMoneyDetailListComponent:React.FC<Props> = ({shareMoneyDetails, 
     const result = 
     <tr>
       <th>선택</th>
-      <th onClick={()=>{changeSort('platform')}}>net/zon</th>
-      <th onClick={()=>{changeSort('rate')}}>비율</th>
-      <th onClick={()=>{changeSort('money')}}>가격</th>
-      <th onClick={()=>{changeSort('accumulation')}}>누적수당</th>
-      <th onClick={()=>{changeSort('status')}}>상태</th>
-      <th onClick={()=>{changeSort('createdDateTime')}}>날짜</th>
+      <th onClick={()=>{changeSort('platform')}}>net/{t("AdminShareMoney:Detail.Filter01")}</th>
+      <th onClick={()=>{changeSort('rate')}}>{t("AdminShareMoney:Detail.Filter02")}</th>
+      <th onClick={()=>{changeSort('money')}}>{t("AdminShareMoney:Detail.Filter03")}</th>
+      <th onClick={()=>{changeSort('accumulation')}}>{t("AdminShareMoney:Detail.Filter04")}</th>
+      <th onClick={()=>{changeSort('status')}}>{t("AdminShareMoney:Detail.Filter05")}</th>
+      <th onClick={()=>{changeSort('createdDateTime')}}>{t("AdminShareMoney:Detail.Filter06")}</th>
     </tr>;
     return result;
   }
