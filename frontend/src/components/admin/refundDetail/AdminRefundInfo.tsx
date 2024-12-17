02bf08d183f1711df344bd20aabbb40ddd2ed25a
JHT <jeonht043@gmail.com>
2024-12-17 14:25:36 +0900

    😎 i18n: Set Language For AdminRefundDetailPage

--- frontend/src/components/admin/refundDetail/AdminRefundInfo.tsx
+++ frontend/src/components/admin/refundDetail/AdminRefundInfo.tsx
@@ -1,17 +1,20 @@
+import { useTranslation } from "react-i18next";
+
 interface Props{
   refundInfo:refundDetail,
 }
 const AdminRefundInfo:React.FC<Props> = ({refundInfo}) => {
+  const { t } = useTranslation();
   return (
     <div>
-      <div><div>요청자</div><div>{refundInfo.name}</div></div>
-      <div><div>전화번호</div><div>{refundInfo.phone}</div></div>
-      <div><div>주문번호</div><div>{refundInfo.orderID}</div></div>
-      <div><div>요청일자</div><div>{refundInfo.createdDateTime}</div></div>
-      <div><div>상태</div><div>{refundInfo.status}</div></div>
-      <div><div>타입</div><div>{refundInfo.type === 100? "반품":"환불"}</div></div>
-      <div><div>상품명</div><div>{refundInfo.productName}</div></div>
-      <div><div>수량</div><div>{refundInfo.quantity}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute01")}</div><div>{refundInfo.name}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute02")}</div><div>{refundInfo.phone}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute03")}</div><div>{refundInfo.orderID}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute04")}</div><div>{refundInfo.createdDateTime}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute05")}</div><div>{refundInfo.status}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute06")}</div><div>{refundInfo.type === 100? t("AdminRefund:Detail.Item01.Option06.Attribute01"):t("AdminRefund:Detail.Item01.Option06.Attribute02")}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute07")}</div><div>{refundInfo.productName}</div></div>
+      <div><div>{t("AdminRefund:Detail.Item01.Attribute08")}</div><div>{refundInfo.quantity}</div></div>
     </div>
   );
 }
