568c368733cf9220795d5380c4b76937eeb0fde1
JHT <jeonht043@gmail.com>
2024-12-17 14:15:04 +0900

    🚀FEAT: Apply Components To Page

--- frontend/src/pages/admin/AdminRefundDetail.tsx
+++ frontend/src/pages/admin/AdminRefundDetail.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AdminRefundInfo } from '../../components';
+import { getRefundDetail_s } from '../../services/customRequest';
 
 const AdminRefundDetail: React.FC = () => {
+  const [refundInfo, setRefundInfo] = useState<refundDetail | undefined>(undefined);
+  const params = useParams<{id:string}>();
+  const getRefundDetail = useCallback (async () => {
+      try {
+          if (params.id !== undefined){
+              const refundDetail : refundDetail = await getRefundDetail_s(params.id);
+              setRefundInfo(refundDetail);
+          }
+      } catch (error) {
+          console.error('Error fetching refundDetail:', error);
+      }
+  }, [params.id]);
+
+  useEffect(() => {
+    getRefundDetail(); // 비동기 함수 호출
+  }, [getRefundDetail]);
+
+  if (!refundInfo) {
+      return (
+          <div>
+              <h1>정보가 없습니다.</h1>
+          </div>
+      );
+  }
+
   return (
-    <div>
-      <h1>AdminRefundDetail</h1>
-    </div>
+      <div>
+          <h1>{refundInfo.refundID}</h1>
+          <AdminRefundInfo refundInfo={refundInfo}/>
+      </div>
   );
 };
 
