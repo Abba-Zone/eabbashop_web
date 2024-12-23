f967a7c42128acc282f4541186b843abca43155d
Cheetozzeong <ych526@naver.com>
2024-12-23 19:01:14 +0900

    🚀 FEAT: add Block duplicate authentication on Email Input & make APIs to process on services

--- frontend/src/services/member.ts
+++ frontend/src/services/member.ts
@@ -1,9 +1,24 @@
-import { login, getUserList, getMemberList, getMemberDetail } from '../apis/memberApi'
+import { login, signup, authEmail, checkAuthCode, checkRecommendEmail, getUserList, getMemberList, getMemberDetail } from '../apis/memberApi'
 
 /*데이터 가공 */
-export const login_s = (loginUser:emailAndPassword):boolean => {
-    login(loginUser);
-    return false;
+export const login_s = async (loginUser:emailAndPassword): Promise<loginSuccess> => {
+    return await login(loginUser);
+};
+
+export const signup_s = async (signupUser: signupUser): Promise<boolean> => {
+    return await signup(signupUser);
+};
+
+export const authEmail_s = async (email: string): Promise<string> => {
+    return await authEmail(email);
+};
+
+export const checkAuthCode_s = async (email: string, code: string): Promise<{ status: number, isValid: boolean }> => {
+    return await checkAuthCode(email, code);
+};
+
+export const checkRecommendEmail_s = async (email: string): Promise<string> => {
+    return await checkRecommendEmail(email);
 };
 
 export const getMemberList_s = async (pageNo:number, pageSize:number, filter:number, filterValue:string, sort:string, sortValue:string): Promise<memberList> => {
