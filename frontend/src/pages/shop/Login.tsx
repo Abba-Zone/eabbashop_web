ba1a4547edfe369c9abee9ac560454aef745ed3d
Cheetozzeong <ych526@naver.com>
2024-12-26 14:54:43 +0900

    🌏 i18N: Process Multilanguage work on Login pages

--- frontend/src/pages/shop/Login.tsx
+++ frontend/src/pages/shop/Login.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react"
 import { login_s } from "../../services/member"
+import { useTranslation } from "react-i18next";
 
 const Login:React.FC = () => {
+  const { t } = useTranslation('Login');
   const [inputId, setInputId] = useState<string>('')
   const [inputPw, setInputPw] = useState<string>('')
   const navigate = useNavigate();
@@ -20,23 +22,23 @@ const Login:React.FC = () => {
     if (loginResult){
       navigate("/");
     } else {
-      alert('로그인에 실패하였습니다.')
+      alert(t('Alert.LoginFailed'))
     }
   }
 
   return (
     <div>
-      <h2>Login</h2>
+      <h2>{t('Title')}</h2>
       <div>
-          <label htmlFor='input_id'>ID : </label>
+          <label htmlFor='input_id'>{t('Attribute01')}</label>
           <input type='text' name='input_id' value={inputId} onChange = {handleInputId} />
       </div>
       <div>
-          <label htmlFor='input_pw'>PW : </label>
+          <label htmlFor='input_pw'>{t('Attribute02')}</label>
           <input type='password' name='input_pw' value={inputPw} onChange = {handleInputPw} />
       </div>
       <div>
-          <button type='button' onClick={onClickLogin}>Login</button>
+          <button type='button' onClick={onClickLogin}>{t('Button.login')}</button>
       </div>
     </div>
   );
