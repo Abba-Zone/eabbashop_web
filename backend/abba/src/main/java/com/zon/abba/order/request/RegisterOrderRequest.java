1ce9784f46a6ad74dd2288d22117a1b9215a292e
rudgns9334 <rudgns9334@gmail.com>
2025-01-09 11:03:58 +0900

    🚀 create order register api

--- backend/abba/src/main/java/com/zon/abba/order/request/RegisterOrderRequest.java
+++ backend/abba/src/main/java/com/zon/abba/order/request/RegisterOrderRequest.java
@@ -1,4 +1,21 @@
 package com.zon.abba.order.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.zon.abba.order.dto.ProductDto;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterOrderRequest {
+    @JsonProperty("addressID")
+    private String addressId;
+    @JsonProperty("billAddressID")
+    private String billAddressId;
+    @JsonProperty("products")
+    private List<ProductDto> products;
 }
