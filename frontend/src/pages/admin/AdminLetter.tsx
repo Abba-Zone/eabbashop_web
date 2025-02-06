6f0180a8e8e844572eecd31e6ad53d20c2e0370a
jht <jeonht043@gmail.com>
2025-02-06 15:21:36 +0900

    FEAT: Develop Change PageSize Function For AdminPage

--- frontend/src/pages/admin/AdminLetter.tsx
+++ frontend/src/pages/admin/AdminLetter.tsx
@@ -80,6 +80,7 @@ const AdminLetter: React.FC = () => {
         <option value={10}>10</option>
         <option value={20}>20</option>
         <option value={30}>30</option>
+        <option value={50}>50</option>
         <option value={100}>100</option>
       </select><span>개씩 보기</span>
       <AdminBoardList boards={letters}  changeSort={changeSort}></AdminBoardList>
